2c368c2e0ff762f7564ac6f8791ef7543ec2bb5b
goby640 <goby640@gmail.com>
2025-04-03 22:28:51 +0300

    Fix links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import texts from "../../public/texts";
 
 const Header = () => {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+
   return (
     <div className="w-full my-4 inset-x-0 flex flex-col justify-center items-center">
-      <a href="/" className="undecorated">
+      <a href={baseUrl} className="undecorated">
         <div className="text-5xl font-bold text-center">
           {texts.HEADER_NAME}
         </div>
