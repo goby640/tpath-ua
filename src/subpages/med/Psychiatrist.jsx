f6d53281bc5b6340d4c2ca79d25931989fef15cb
goby640 <goby640@gmail.com>
2025-04-04 03:59:29 +0300

    Add stub for article page

--- src/subpages/med/Psychiatrist.jsx
+++ src/subpages/med/Psychiatrist.jsx
@@ -1,7 +1,11 @@
+import texts from "../../../public/texts";
+
 const Psychiatrist = () => {
     return (
-        <div>
-            STUB
+        <div className=" w-full h-fit max-h-fit max-w-3xl p-6">
+            <div className="text-left display-linebreak p-4">
+                {texts.PSYCHIATRIST_TEXT}
+            </div>
         </div>
     );
 };
