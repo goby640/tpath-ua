6fde118e3dbca8190922550b213ea9c5a7053371
goby640 <goby640@gmail.com>
2025-04-05 23:15:41 +0300

    Prettify styles of  elements in tldr card

--- src/components/TldrCard.jsx
+++ src/components/TldrCard.jsx
@@ -3,12 +3,12 @@ const TldrCard = ({card}) => {
     return (
         <div className="p-4 mb-8 grid grid-cols-1 gap-4 md:grid-cols-2 w-full shadow-lg rounded-lg bg-white/70 text-black justify-stretch">
             <div className="w-full flex flex-rows-2 items-center">
-                <div className="w-1/10 max-w-1/10 min-w-1/10 mr-2 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
+                <div className="self-start w-1/10 max-w-1/10 min-w-1/10 mr-4 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
                     <img className="p-1"
                         src={baseUrl + "/input.png"}
                         alt="input"/>
                 </div>
-                <ul className="pl-4 list-disc">
+                <ul className="self-start pl-4 list-disc">
                     {
                         card.input.map((element, index) => {
                             return (<li key={index}>{element}</li>)
@@ -17,12 +17,12 @@ const TldrCard = ({card}) => {
                 </ul>
             </div>
             <div className="w-full flex flex-rows-2 items-center">
-                <div className="w-1/10 max-w-1/10 min-w-1/10 mr-2 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
+                <div className="self-start w-1/10 max-w-1/10 min-w-1/10 mr-4 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
                     <img className="p-1"
                         src={baseUrl + "/output.png"}
                         alt="output"/>
                 </div>
-                <ul className="pl-4 list-disc">
+                <ul className="self-start pl-4 list-disc">
                     {
                         card.output.map((element, index) => {
                             return (<li key={index}>{element}</li>)
@@ -31,7 +31,7 @@ const TldrCard = ({card}) => {
                 </ul>
             </div>
             <div className="w-full flex flex-rows-2 items-center">
-                <div className="w-1/10 max-w-1/10 min-w-1/10 mr-2 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
+                <div className="self-start w-1/10 max-w-1/10 min-w-1/10 mr-4 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
                     <img className="p-1"
                         src={baseUrl + "/time.png"}
                         alt="time"/>
@@ -39,7 +39,7 @@ const TldrCard = ({card}) => {
                 <p>{card.time}</p>
             </div>
             <div className="w-full flex flex-rows-2 items-center">
-                <div className="w-1/10 max-w-1/10 min-w-1/10 mr-2 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
+                <div className="self-start w-1/10 max-w-1/10 min-w-1/10 mr-4 relative aspect-square shadow-lg rounded-lg bg-white/50 overflow-hidden">
                     <img className="p-1"
                         src={baseUrl + "/price.png"}
                         alt="price"/>
